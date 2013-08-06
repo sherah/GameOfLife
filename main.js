51a457597d616304000db6068cc862aadf3ee043
sherah <sherah@sherahsmith.com>
2013-08-06 11:10:36 -0700

    shaping algorithm

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ $(document).ready(function(){
     var numberOfIterations = $('#numberOfIterations').val();
         gridWidth          = $('#gridWidth').val();
         gridHeight         = $('#gridHeight').val();
-        gridValues         = $('#gridValues').val();
+        gridValues         = $('#gridValues').val(); //TODO: refactor to accept lines when making compatible with stdin/stdout
 
     //kick off the algorithm for the game
-    var results = runGame(numberOfIterations, gridHeight, gridWidth);
+    var results = runGame(numberOfIterations, gridHeight, gridWidth, values);
 
     //display the results
     displayGame(results);
@@ -20,10 +20,24 @@ $(document).ready(function(){
 
   });
 
-  var runGame = function(n, h, w){
+  var runGame = function(n, h, w, v){
     var result = [];
 
-    //algorithm here
+    //do this n times
+    for(var i = 0; i <= n; i++){
+
+      //if this index's value is 0
+        //get all its neighbor's values
+          //if there are exactly 3 1's, turn this index into a 1
+
+
+
+      //if this index's value is 1
+        //get all its neighbor's values
+          //if there are 2 or 3 neighbors, stay a 1
+          //else change to 0
+
+    }
 
     return result;
   };
